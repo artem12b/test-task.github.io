603d6392407f38d64153285461164a5696e6bacf
Artem Belozerov <95310978+artem12b@users.noreply.github.com>
2024-02-07 18:28:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/REPONAME/",
+  base: "/test-task.github.io/",
   plugins: [
     vue(),
     vueJsx(),
